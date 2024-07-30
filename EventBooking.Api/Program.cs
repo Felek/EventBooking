15b69cb5285874d5e08baffd90d94ac2abc28e19
Tomasz Guźniczak <tomguz@o2.pl>
2024-07-30 02:08:11 +0200

    always turn on swagger

--- EventBooking.Api/Program.cs
+++ EventBooking.Api/Program.cs
@@ -37,11 +37,9 @@ builder.Services.AddSwaggerGen(o =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
